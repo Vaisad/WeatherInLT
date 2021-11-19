3a1b7aea7399ae215ba5cb3ba658a947b6c3cda4
Vaidas Sauklys <76744233+Vaisad@users.noreply.github.com>
2021-11-19 22:04:57 +0100

    NewProject

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,30 +1,28 @@
 // window.axios = require('axios');
 // window.bootstrap = require('bootstrap');
+
+
 const search = document.querySelector('.button-search');
 const menu = document.querySelector('.button-menu');
 const input = document.querySelector('.input');
 const form = document.querySelector('.form-form');
 const close = document.querySelector('.close-img');
+const menuButton = document.querySelector('#menu');
 
 
-menu.addEventListener('click', () => form.style.display = 'inline');
-close.addEventListener('click', () => form.style.display = 'none');
-
-let bodyClick = true;
-window.addEventListener('click', function () {
-    bodyClick = !bodyClick;
-    if (!bodyClick) {
-        form.style.display = 'none';
-    }
+menu.addEventListener('click', () => form.classList.toggle('d-none'));
+close.addEventListener('click', () => form.classList.toggle('d-none'));
+document.addEventListener('click', (e) => {
+    if (!menuButton.contains(e.target)) {
+        form.classList.add('d-none');
+    };
 });
 
-let temp = true;
 search.addEventListener('click', function () {
-    temp = !temp;
-    if (!temp) {
-        input.style.display = "inline"
+    if (input.classList.contains('d-none')) {
+        input.classList.toggle('d-none');
     } else {
-        input.style.display = "none"
+        getWeather(input.value);
     }
 });
 
@@ -42,34 +40,33 @@ function setParametersOnPage(city, airTemperature, totalPrecipitation, windSpeed
     let degrees = document.querySelector('.degrees');
     let windNumber = document.querySelector('.wind_number');
     let milimetersNumbers = document.querySelector('.milimeters_numbers');
-    let smallPerHour = document.querySelector('.smallPerHour');
     cityPlace.textContent = city;
     degrees.textContent = airTemperature;
     windNumber.textContent = windSpeed;
     milimetersNumbers.textContent = totalPrecipitation;
-    smallPerHour.textContent = windGust;
 }
 
 
 function getWeather(city) {
     fetchWeatherByCity(city).then(response => {
 
-        setParametersOnPage(response.place.name, response.forecastTimestamps[1].airTemperature, response.forecastTimestamps[1].totalPrecipitation, response.forecastTimestamps[1].windGust, response.forecastTimestamps[1].windSpeed);
-
+        setParametersOnPage(response.place.name, response.forecastTimestamps[1].airTemperature, response.forecastTimestamps[1].totalPrecipitation, response.forecastTimestamps[1].windSpeed);
     })
 };
+
 
 input.addEventListener('keyup', logKey);
 
 function logKey(e) {
     const value = input.value;
     if (e.key === 'Enter' || e.keyCode === 13) {
+
         getWeather(value);
     }
 };
 
-//-----------------------------------------------------
 
+//-----------------------------------------------------
 
 
 
